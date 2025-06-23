9949c80ee45a65fa03aa0f21741be3d0c2f714fc
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-06-23 10:05:21 -0300

    up

---
+++ dom/index4.js
@@ -0,0 +1,5 @@
+document.title = "javascript alterando dom";
+// document.body.style.backgroundColor = "blue";
+// document.body.style.backgroundImage = "url('./assets/img.webp')";
+
+document.body.innerHTML="inserçao basica"
