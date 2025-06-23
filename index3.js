fcf3adfdd28a301747d306fa2a41171aaa6d4bbf
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-06-22 21:06:57 -0300

    up

---
+++ index3.js
@@ -0,0 +1,6 @@
+const cursos = ["html","js","php","angular"]
+let c = cursos.map((element,index)=>{
+	// console.log("curso " + element + " posiçao "+ index)
+	return "<div>"+element+"<div>"
+})
+console.log(c)
