5e96ead40a6fa2df9b0d174139712a453c9eccbb
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-11-04 22:54:39 -0300

    up

--- metodos/ex1.js
+++ metodos/ex1.js
@@ -8,3 +8,13 @@ const maioresDeVinte = idades.filter((value)=>{
 });
 console.log(idades)	
 console.log(maioresDeVinte)
+
+const idades2 = [12, 15, 20, 25, 30, 35, 40];
+
+const menores =  idades2.filter((value)=>{
+	if (value<18){
+		return value
+	}
+})
+console.log(idades2)	
+console.log(menores)
